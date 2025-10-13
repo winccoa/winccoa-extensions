e7ea3629ea95e9cfc64903e4ac17efa02fe3321d
Schulz Jonas <jonas.schulz@etm.at>
2025-10-13 15:44:27 +0200

    Initial java-script project structure

---
+++ winccoa-marketplace/javascript/winccoa-marketplace/eslint.config.mjs
@@ -0,0 +1,50 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/node_modules",
+      "**/dist",
+      "**/winccoa-manager",
+      "**/templates",
+      "**/@types",
+      "**/tempWidget",
+      "rollup.config.mjs",
+    ],
+  },
+  {
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["eslint.config.mjs"],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: { ...globals.browser, ...globals.node },
+    },
+  },
+  {
+    files: ["system-dashboard/**"],
+    rules: {
+      "@typescript-eslint/no-redundant-type-constituents": "error",
+      "require-await": "error",
+      "no-await-in-loop": "error",
+      "no-constructor-return": "error",
+      "no-duplicate-imports": "error",
+      "no-template-curly-in-string": "error",
+      "no-promise-executor-return": "error",
+    },
+  },
+  {
+    // disable type-aware linting on JS files
+    files: ["**/*.js"],
+    ...tseslint.configs.disableTypeChecked,
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+);
