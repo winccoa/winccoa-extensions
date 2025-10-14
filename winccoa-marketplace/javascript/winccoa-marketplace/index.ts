b54c8c6a076a6b8c8e351845b952da325ec5ed41
DojimaNoRyu <98286304+DojimaNoRyu@users.noreply.github.com>
2025-10-14 10:57:36 +0200

    Add Server functions
    * add AddOnHandler
    
    * add pull function
    
    * change toString to use VRPC getString
    
    ---------
    
    Co-authored-by: Stojnic Daniel <daniel.stojnic@etm.at>

--- winccoa-marketplace/javascript/winccoa-marketplace/index.ts
+++ winccoa-marketplace/javascript/winccoa-marketplace/index.ts
@@ -7,12 +7,11 @@ const marketplaceService = new MarketplaceService();
 async function main() {
   try {
     console.log("Starting Marketplace Service...");
-    
+
     container.registerService(marketplaceService);
     container.startAllServices();
-    
+
     console.log("Marketplace Service created and registered successfully");
-    
   } catch (error) {
     console.error("Failed to start Marketplace Service:", error);
   }
--- winccoa-marketplace/javascript/winccoa-marketplace/lib/Service.ts
+++ winccoa-marketplace/javascript/winccoa-marketplace/lib/Service.ts
@@ -1,46 +1,116 @@
 import { Vrpc } from "winccoa-manager";
-import { GitHandler } from "./GitHandler";
+import { AddOnHandler } from "./AddOnHandler";
 
 export class MarketplaceService extends Vrpc.ServiceBase {
-  private gitHandler: GitHandler;
+  private _addOnHandler: AddOnHandler;
 
   constructor() {
     super("Marketplace");
 
-    this.registerFunction("pull", this.pull.bind(this));
-    this.registerFunction("clone", this.clone.bind(this));
-    this.registerFunction("listRepos", this.listRepos.bind(this));
+    this.registerFunction("register", this.registerSubProject.bind(this));
+    this.registerFunction("unregister", this.unregisterSubProject.bind(this));
+    this.registerFunction(
+      "listProjects",
+      this.listRegisteredSubProjects.bind(this),
+    );
 
-    this.gitHandler = new GitHandler();
+    this.registerFunction("pull", this.pullRepository.bind(this));
+    this.registerFunction("clone", this.cloneRepository.bind(this));
+    this.registerFunction("listRepos", this.listRemoteRepositories.bind(this));
+
+    this._addOnHandler = new AddOnHandler();
   }
 
-  private async pull(
+  private async registerSubProject(
     serverContext: Vrpc.ServerContext,
     request: Vrpc.Variant,
   ): Promise<Vrpc.Variant> {
     return Vrpc.Variant.createUndefined();
   }
 
-  private async clone(
+  private async unregisterSubProject(
     serverContext: Vrpc.ServerContext,
     request: Vrpc.Variant,
   ): Promise<Vrpc.Variant> {
-
     return Vrpc.Variant.createUndefined();
   }
 
-  private async listRepos(
+  private async listRegisteredSubProjects(
     serverContext: Vrpc.ServerContext,
     request: Vrpc.Variant,
   ): Promise<Vrpc.Variant> {
-    const orgRepos = await this.gitHandler.listOrganizationRepositories('winccoa', {
-      type: 'public',
-      sort: 'updated',
-      direction: 'desc'
-    });
+    return Vrpc.Variant.createUndefined();
+  }
+
+  private async listRemoteRepositories(
+    serverContext: Vrpc.ServerContext,
+    request: Vrpc.Variant,
+  ): Promise<Vrpc.Variant> {
+    const organizationName = request.getString();
+    console.log("Fetching repositories for organization:", organizationName);
+
+    const orgRepos = await this._addOnHandler.listOrganizationRepositories(
+      organizationName,
+      {
+        type: "public",
+        sort: "updated",
+        direction: "desc",
+      },
+    );
 
     console.log("Fetched repositories:", orgRepos);
 
-    return Vrpc.Variant.createString(orgRepos.toString());
+    return Vrpc.Variant.createString(JSON.stringify(orgRepos));
+  }
+
+  private async pullRepository(
+    serverContext: Vrpc.ServerContext,
+    request: Vrpc.Variant,
+  ): Promise<Vrpc.Variant> {
+    const directory = request.getString();
+    console.log("Pulling latest changes in directory:", directory);
+
+    await this._addOnHandler.pullRepository(directory);
+
+    return Vrpc.Variant.createUndefined();
+  }
+
+  private async cloneRepository(
+    serverContext: Vrpc.ServerContext,
+    request: Vrpc.Variant,
+  ): Promise<Vrpc.Variant> {
+    let repositoryURL: string;
+    let targetDir: string | undefined;
+    let branch: string | undefined;
+
+    // Get the mapping object - using cast since TypeScript doesn't know about asMapping
+    const mapping = request.getMapping();
+    // Extract URL from mapping
+    const urlVariant = mapping.get(Vrpc.Variant.createString("url"));
+    if (!urlVariant) {
+      throw new Error('Missing required "url" parameter in mapping');
+    }
+    repositoryURL = urlVariant.getString();
+
+    // Extract optional parameters from mapping
+    const branchVariant = mapping.get(Vrpc.Variant.createString("branch"));
+    const targetDirVariant = mapping.get(
+      Vrpc.Variant.createString("targetDirectory"),
+    );
+
+    targetDir = targetDirVariant ? targetDirVariant.getString() : undefined;
+    branch = branchVariant ? branchVariant.getString() : undefined;
+
+    console.log("Cloning repository:", repositoryURL);
+    if (targetDir) console.log("Target directory:", targetDir);
+    if (branch) console.log("Branch:", branch);
+
+    await this._addOnHandler.cloneRepositoryFromUrl(
+      repositoryURL,
+      targetDir,
+      branch,
+    );
+
+    return Vrpc.Variant.createUndefined();
   }
 }
